1f4090d1eff2a9ba7d35c6273420ff3d4111223f
towhid <ktowhid20@gmail.com>
2024-01-02 01:15:38 +0600

    initial downloader

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/KnockOutEZ/wisty-go
+
+go 1.21.5
+
+require (
+	github.com/cheggaaa/pb/v3 v3.1.4
+	github.com/urfave/cli v1.22.14
+)
+
+require github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.27.1
+	golang.org/x/sys v0.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/cheggaaa/pb/v3"
+	"github.com/urfave/cli"
+)
+
+func downloadVideo(link, filename string) error {
+	fmt.Printf("%s\n", filename)
+	output, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	response, err := http.Get(link)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	bar := pb.Full.Start64(response.ContentLength)
+	defer bar.Finish()
+
+	// Wrap the response.Body with a proxy reader to update the progress bar
+	reader := bar.NewProxyReader(response.Body)
+
+	_, err = io.Copy(output, reader)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("\nDownload complete!")
+	return nil
+}
+
+func parseResolution(metadata, resolution, filename string) error {
+	file, err := os.Open(metadata)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var res []map[string]interface{}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&res)
+	if err != nil {
+		return err
+	}
+
+	resolutionMapping := map[string]int{
+		"1080p": 1080,
+		"720p":  720,
+		"540p":  540,
+		"480p":  480,
+		"360p":  360,
+	}
+
+	selectedResolution := resolutionMapping[resolution]
+	var videoURL string
+
+	for _, v := range res {
+		if int(v["height"].(float64)) == selectedResolution {
+			videoURL = v["url"].(string)
+			break
+		}
+	}
+
+	return downloadVideo(videoURL, filename+".mp4")
+}
+
+func fetchResolutions(id, resolution, filename string) error {
+	fmt.Println("Connecting...\n")
+	url := "http://fast.wistia.net/embed/iframe/" + id
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	var content []map[string]interface{}
+	re := regexp.MustCompile(`"assets":(\[.*?\])`)
+	match := re.FindStringSubmatch(string(body))
+
+	if len(match) > 1 {
+		if err := json.Unmarshal([]byte(match[1]), &content); err != nil {
+			return err
+		}
+
+		withExtract := "extract.json"
+		withExtractPath := filepath.Join(".", withExtract)
+
+		file, err := os.Create(withExtractPath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		encoder := json.NewEncoder(file)
+		if err := encoder.Encode(content); err != nil {
+			return err
+		}
+
+		if err := parseResolution(withExtract, resolution, filename); err != nil {
+			return err
+		}
+
+		if err := os.Remove(withExtractPath); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "wisty"
+	app.Usage = "Wistia video downloader command line tool"
+	app.Version = "1.0.0"
+
+	var id, resolution, name string
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "id, i",
+			Usage:       "Wistia video id",
+			Destination: &id,
+		},
+		cli.StringFlag{
+			Name:        "resolution, r",
+			Value:       "1080p",
+			Usage:       "Video resolution (e.g., 720p)",
+			Destination: &resolution,
+		},
+		cli.StringFlag{
+			Name:        "name, n",
+			Value:       "video",
+			Usage:       "Video name",
+			Destination: &name,
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if id == "" {
+			return cli.NewExitError("Missing required argument 'id'. Run 'wisty --help' for help.", 1)
+		}
+
+		return fetchResolutions(id, resolution, name)
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
